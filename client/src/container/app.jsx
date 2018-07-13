9a1353bc7db867e343385d39c36a0ef01256dcec
jameshih <j@shih.app>
2018-07-12 22:31:17 -0400

    update

--- client/src/container/app.jsx
+++ client/src/container/app.jsx
@@ -1,120 +1,102 @@
-import React, { Component }                 from 'react';
-import { Route, Redirect } from 'react-router'
-import { Button,
-    		 Form,
-    		 Col,
-    		 Column,
-    		 Row,
-    		 FormGroup,
-    		 ControlLabel,
-         Navbar,
-         Nav,
-         NavItem,
-         NavDropdown,
-         MenuItem}					 from 'react-bootstrap';
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router";
+import {
+  Button,
+  Form,
+  Col,
+  Column,
+  Row,
+  FormGroup,
+  ControlLabel,
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem
+} from "react-bootstrap";
 
-import Dropzone from 'react-dropzone'
+import Dropzone from "react-dropzone";
 
 import SimpleSlider from "../microcomponents/slider";
 
-
-
 class App extends Component {
-
   state = {
     showTreasure: false
-  }
+  };
 
   componentDidMount() {
     // NOTE: Jimmy  put call here
   }
 
-  onDrop = (files) => {
-
-    console.log(files)
+  onDrop = files => {
+    console.log(files);
 
     const formData = new FormData();
 
     //todo: roll in a map
-    formData.append('file', files[0]);
+    formData.append("file", files[0]);
 
     uploadWireframe(formData);
 
-
     //temp approach... change to event driven approach
     setTimeout(() => {
-      console.log("refreshing if it was event based...")
-      getStartingObj()
-    }, 10000)
-
-
-
-  }
+      console.log("refreshing if it was event based...");
+      getStartingObj();
+    }, 10000);
+  };
 
   checkLogin = () => {
-    if(!localStorage.loggedIn){
-      return <Redirect to="/login"/>
+    if (!localStorage.loggedIn) {
+      return <Redirect to="/login" />;
     }
-  }
+  };
 
   render() {
+    return (
+      <div>
+        {this.checkLogin()}
 
-    return(
-        <div>
-          {this.checkLogin()}
+        {this.showTreasure ? (
+          <div>
+            <SimpleSlider />
+            <Dropzone className="dropzone" onDrop={this.onDrop.bind(this)}>
+              <h5>Upload stuff</h5>
+              <p />
+            </Dropzone>
+          </div>
+        ) : (
+          <div className="app">
+            <Row>
+              <Col md={12} className="titleName">
+                WILMA
+              </Col>
+            </Row>
+            <Row>
+              <Col md={1} className="bubblesDiv">
+                bubles
+              </Col>
+              <Col md={5} className="willOverview">
+                will OverView
+              </Col>
+              <Col md={5}>
+                <Row className="digitalShit">digital Shit</Row>
+                <Row className="memories">Memories</Row>
+              </Col>
+            </Row>
 
-
-          {this.showTreasure ?
-            <div>
-              <SimpleSlider/>
-              <Dropzone className="dropzone" onDrop={this.onDrop.bind(this)}>
-                <h5>Upload stuff</h5>
-                <p></p>
-              </Dropzone>
-            </div>
-            :
-            <div className='app'>
-              <Row>
-                <Col md={12} className="titleName">
-                  WILMA
-                </Col>
-              </Row>
-              <Row>
-                <Col md={1} className="bubblesDiv">
-                  bubles
-                </Col>
-                <Col md={5} className="willOverview">
-                  will OverView
-                </Col>
-                <Col md={5} >
-                  <Row className="digitalShit">
-                    digital Shit
-                  </Row>
-                  <Row className="memories">
-                    Memories
-                  </Row>
-                </Col>
-              </Row>
-
-              <Row>
-                <Col md={5} mdOffset={1} className="willDetails">
-                  will
-                </Col>
-                <Col md={1} mdOffset={8} className="idkButton">
-                  button
-                </Col>
-              </Row>
-
-            </div> }
-
-
-
-
-        </div>
-
-        )
-
+            <Row>
+              <Col md={5} mdOffset={1} className="willDetails">
+                will
+              </Col>
+              <Col md={1} mdOffset={8} className="idkButton">
+                button
+              </Col>
+            </Row>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
---
+++ client/src/utils/ipfs.js
@@ -0,0 +1,3 @@
+const IPFS = require("ipfs-api");
+const ipfs = IPFS("ipfs.infura.io", "5001", { protocol: "https" });
+const ipfsAddress = "QmbkpFCxowU4tznBi344KubBtuDfVGbPrBtt8a8H1LDd83";
