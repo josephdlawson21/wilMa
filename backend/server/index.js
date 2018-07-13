3bea477711554c557e41cfef2ee785d2bcc27a8f
jameshih <j@shih.app>
2018-07-13 01:13:12 -0400

    update

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,6 +1,6 @@
 const IPFS = require("ipfs-api");
 const ipfs = IPFS("ipfs.infura.io", "5001", { protocol: "https" });
-const fileName = "template.json";
+const fileName = "sound.mp3";
 const fs = require("fs"),
   path = require("path"),
   filePath = path.join(__dirname, fileName);
@@ -10,8 +10,8 @@ const uploader = _path => {
   content = new Buffer(content);
   ipfs.files.add(content, function(err, res) {
     console.log(err, res);
-    //return res[0].path;
-    getter(res[0].path);
+    return res[0].path;
+    // getter(res[0].path);
   });
 };
 
